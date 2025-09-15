92ed10a8d19dfa074a53c66207edaf85a48f23e3
golihaidari <golihaidari@gmail.com>
2025-09-15 11:15:38 +0200

    edit vite config and yml file.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/BidGallery/' : '/', // dev uses '/'
+  //base: command === 'build' ? '/BidGallery/' : '/', // dev uses '/'
+  base: './', // relative paths for GitHub Pages SPA
 }))
