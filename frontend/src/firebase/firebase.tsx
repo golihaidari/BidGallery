a38a92fa2e397c57777d290a74b7aafca716b1d2
golihaidari <golihaidari@gmail.com>
2025-11-08 00:26:31 +0100

    triger with fixing firbase-config infrontend

--- frontend/src/firebase/firebase.tsx
+++ frontend/src/firebase/firebase.tsx
@@ -1,12 +1,15 @@
 import {initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
+import firebaseConfig from "../../firebase.config.json";
+
+/*const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+};*/
+
 
 // Initialize Firebase only if it hasn’t been initialized already
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
