86582cbeb5f60c1ae8294b3bb10be3036b1d9719
golihaidari <golihaidari@gmail.com>
2025-11-12 14:47:50 +0100

    triger 5-12

--- backend/src/main/java/dk/dtu/backend/controller/AddressController.java
+++ backend/src/main/java/dk/dtu/backend/controller/AddressController.java
@@ -24,7 +24,7 @@ public class AddressController {
     @Autowired
     private AddressService addressService;
 
-    // -------------------CREATE----------------------------triger 4-12 
+    // -------------------CREATE----------------------------triger 5-12 
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER')")
     public ResponseEntity<Address> createAddress(@RequestBody Address address) {
