262314d80dc7c519aa6ec2bfc390a60d5f401723
golihaidari <golihaidari@gmail.com>
2025-11-13 13:41:48 +0100

    trigger 9-13

--- backend/src/main/java/dk/dtu/backend/controller/AddressController.java
+++ backend/src/main/java/dk/dtu/backend/controller/AddressController.java
@@ -24,7 +24,7 @@ public class AddressController {
     @Autowired
     private AddressService addressService;
 
-    // -------------------CREATE----------------------------triger 8-13 
+    // -------------------CREATE----------------------------triger 9-13 
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER')")
     public ResponseEntity<Address> createAddress(@RequestBody Address address) {
