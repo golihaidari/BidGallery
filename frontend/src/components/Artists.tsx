16eaa777406cb59f6364fd94db1bbbcf77e90c60
golihaidari <golihaidari@gmail.com>
2025-11-17 12:20:28 +0100

    trigger 17-2

--- frontend/src/components/Artists.tsx
+++ frontend/src/components/Artists.tsx
@@ -8,7 +8,7 @@ import "./extraCss/Artists.css";
 import { API_CONFIG } from "../config";
 
 
-//const dataUrl = "/data/artists.json"; triger 13-14
+//const dataUrl = "/data/artists.json"; triger 17.2
 
 const dataUrl =`${API_CONFIG.baseURL}/artists`
 
